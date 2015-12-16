91c7016922ade76456d66fddf8fc8f861856070c
Freedom <510885304@qq.com>
2015-12-16 17:24:49 +0800

    o

---
+++ controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\User;
+
+class LoginController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        $name = $_REQUEST['name'];
+        $password = $_REQUEST['password'];
+    }
+}
---
+++ models/User.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $password
+ * @property integer $update_at
+ * @property integer $create_at
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'password', 'update_at', 'create_at'], 'required'],
+            [['update_at', 'create_at'], 'integer'],
+            [['name'], 'string', 'max' => 32],
+            [['rand'], 'string', 'max' => 8],
+            [['password'], 'string', 'max' => 128]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'rand' => 'Rand',
+            'password' => 'Password',
+            'update_at' => 'Update At',
+            'create_at' => 'Create At',
+        ];
+    }
+
+    public static function login($name, $password)
+    {
+        $user = self::findUserByName($name);
+        if(null != $user)
+        {
+            if($user->validatePassword($password))
+            {
+                return Yii::$app->user->login($user);
+            }
+        }
+        return false;
+    }
+
+    public function findUserByName($name)
+    {
+        return User::find()->where(['name'=>$name])->one();
+    }
+
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+
+
+
+
+    /**
+     * Finds an identity by the given ID.
+     * @param string|integer $id the ID to be looked for
+     * @return IdentityInterface the identity object that matches the given ID.
+     * Null should be returned if such an identity cannot be found
+     * or the identity is not in an active state (disabled, deleted, etc.)
+     */
+    public static function findIdentity($id)
+    {
+//        return static::findOne($id);
+    }
+
+    /**
+     * Finds an identity by the given token.
+     * @param mixed $token the token to be looked for
+     * @param mixed $type the type of the token. The value of this parameter depends on the implementation.
+     * For example, [[\yii\filters\auth\HttpBearerAuth]] will set this parameter to be `yii\filters\auth\HttpBearerAuth`.
+     * @return IdentityInterface the identity object that matches the given token.
+     * Null should be returned if such an identity cannot be found
+     * or the identity is not in an active state (disabled, deleted, etc.)
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return;
+    }
+
+    /**
+     * Returns an ID that can uniquely identify a user identity.
+     * @return string|integer an ID that uniquely identifies a user identity.
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns a key that can be used to check the validity of a given identity ID.
+     *
+     * The key should be unique for each individual user, and should be persistent
+     * so that it can be used to check the validity of the user identity.
+     *
+     * The space of such keys should be big enough to defeat potential identity attacks.
+     *
+     * This is required if [[User::enableAutoLogin]] is enabled.
+     * @return string a key that is used to check the validity of a given identity ID.
+     * @see validateAuthKey()
+     */
+    public function getAuthKey()
+    {
+        return;
+    }
+
+    /**
+     * Validates the given auth key.
+     *
+     * This is required if [[User::enableAutoLogin]] is enabled.
+     * @param string $authKey the given auth key
+     * @return boolean whether the given auth key is valid.
+     * @see getAuthKey()
+     */
+    public function validateAuthKey($authKey)
+    {
+        return;
+    }
+}
