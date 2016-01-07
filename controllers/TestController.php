99474e47e4fede4697a145cecb7809704d9d97ee
freedom <510885304@qq.com>
2016-01-07 22:02:57 +0800

    o

---
+++ controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+class TestController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionUpload()
+    {
+        echo 'upload';
+    }
+}
---
+++ views/test/index.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<script type="text/javascript" src="/ckeditor/ckeditor.js"></script>
+<form action="test/upload" method="post">
+<textarea id="editor1" name="editor1" ></textarea>
+</form>
+<script>
+    CKEDITOR.replace( 'editor1', {
+        extraPlugins: 'mathjax',
+        mathJaxLib: 'http://cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML',
+        height: 320
+    } );
+
+    if ( CKEDITOR.env.ie && CKEDITOR.env.version == 8 ) {
+        document.getElementById( 'ie8-warning' ).className = 'tip alert';
+    }
+</script>
